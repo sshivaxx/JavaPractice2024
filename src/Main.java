8b5f4db0a447b535ad5cd225e5cc44c578c1bb17
Mikhail Charniak <132018645+sshivaxx@users.noreply.github.com>
2024-03-31 22:35:32 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,5 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
         System.out.println("Hello world!");
-        final int x = 25;
-        System.out.println(x);
-        int inp = in.nextInt();
-        in.close();
-        System.out.println(inp);
-
     }
 }
